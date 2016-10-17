c208bdbe4385c6118cf0d74d66916838b63e9304
Yasset Perez-Riverol <ypriverol@gmail.com>
2016-10-17 01:48:05 +0100

    new version of the biocontainers-registry

--- dist/script.js
+++ dist/script.js
@@ -14,10 +14,36 @@
  * limitations under the License.
  */
 
-var app = angular.module('DockerWebUI', ['ngCookies','ngRoute','ngClipboard']);
+var dockerhub_registry = "https://hub.docker.com/v2/repositories/biodckr/";
+var cross_proxy        = "https://crossorigin.me/";
+var quay_registry      = "https://quay.io/api/v1/repository?namespace=biocontainers&popularity=true&last_modified=true";
+var quay_registry_repo = "https://quay.io/api/v1/repository/";
+
+var dockerhub_command  = "biodckr/";
+var quay_organization       = "biocontainers/";
+
+var app = angular.module('DockerWebUI', ['ngCookies','ngRoute','ngClipboard', 'siTable','ngDonut', 'hljs']);
+
+angular.module('DockerWebUI')
+    .factory('timeoutHttpIntercept', function ($rootScope, $q) {
+        return {
+            'request': function(config) {
+                config.timeout = 100000;
+                return config;
+            }
+        }});
+
 app.config(['ngClipProvider', function(ngClipProvider) { ngClipProvider.setPath("includes/bower_components/zeroclipboard/dist/ZeroClipboard.swf"); }]);
+
 app.config(function($httpProvider){
 	delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
+
+app.config(function (hljsServiceProvider) {
+    hljsServiceProvider.setOptions({
+        // replace tab with 4 spaces
+        tabReplace: '    '
+    });
 });
 app.config(function($routeProvider) {
 	$routeProvider
@@ -29,263 +55,138 @@ app.config(function($routeProvider) {
 			templateUrl : 'pages/showNamespaces.html',
 			controller  : 'NamespacesController'
 		})
-		.when('/showNamespacesRepos', {
-			templateUrl : 'pages/showNamespacesRepos.html',
-			controller  : 'NamespacesReposController'
-		})
-		.when('/showRepositories', {
-			templateUrl : 'pages/showRepositories.html',
-			controller  : 'RepositoriesController'
-		})
 		.when('/showImages', {
 			templateUrl : 'pages/showImages.html',
 			controller  : 'ImagesController'
 		});
 });
+
 app.controller('MainController', ['$scope','$route','$window','$cookies','$location',function($scope,$route,$window,$cookies,$location) {
-	var paramIP = $location.search()['IP'] || '';
-	var paramProtocol = $location.search()['protocol'] || 'http';
-	if(paramIP!=='')
-	{
-		$cookies.IP=paramIP;
-		$cookies.protocol=paramProtocol;
-		console.log('the ip has been changed to ' + $cookies.protocol + '://' + $cookies.IP);
-		$window.location.href = "#showNamespaces";
-		$route.reload();
-	}
-	$scope.ip = paramIP;
-	$scope.protocol = paramProtocol;
-	$scope.getIP=function()
-	{
-		$scope.ip = $cookies.IP || '';
-		$scope.protocol = $cookies.protocol || 'http';
-		console.log('getIP the current IP has been to '+$cookies.IP);
-	}
-	$scope.setIP=function()
-	{
-		$cookies.IP=$scope.ip;
-		$cookies.protocol=$scope.protocol;
-		console.log('the IP has been changed to '+$cookies.IP);
-		console.log('the protocol has been changed to '+$cookies.protocol);
+	var paramIP = dockerhub_registry;
+	if(paramIP!=='') {
+		console.log('the ip has been changed to ' + paramIP);
 		$window.location.href = "#showNamespaces";
 		$route.reload();
 	}
 }]);
+
 app.controller('NamespacesController', function($rootScope,$scope,$http,$route,$cookies,$window,$location) {
-	$scope.IP=$cookies.IP;
-	$scope.protocol=$cookies.protocol;
-	if($scope.IP===undefined)
-	{
+
+	if(dockerhub_registry === undefined) {
 		$window.location.href="#";
 		$route.reload();
-	}
-	else
-	{
-		console.log('the ip is ' + $scope.IP);
+	} else {
+		console.log('the ip is ' + dockerhub_registry);
+        $scope.IP = dockerhub_registry;
 		$scope.num_results=0;
 		$scope.dictionary={};
 		$scope.namespacesList=[];
 		results=[];
-		$http({method: 'GET', url: $scope.protocol+'://'+$scope.IP+'/v1/search'}).success(function(data)
-		{
-			$scope.num_results=data.num_results;
-			results=data.results;
-			angular.forEach(results,function(result)
-			{
-				$scope.dictionary[result.name.split('/')[0]]=[];			
-			});
-			angular.forEach(results,function(result)
-			{
-				$scope.dictionary[result.name.split('/')[0]].push(result.name.split('/')[1]);
-			});	
-			angular.forEach($scope.dictionary,function(key,value)
-			{
-				temp={};
-				temp['name']=value;
-				$scope.namespacesList.push(temp);
-			});
-		}).error(function(data){alert('Unable to reuest.')});
+		$scope.num_results = 0;
+		retrieveDockerHub(cross_proxy + dockerhub_registry, $http, $scope);
+		retrieveQuayIO(  quay_registry, $http, $scope);
+		console.log($scope.num_results)
 	}
 });
-app.controller('NamespacesReposController', function($rootScope,$scope,$http,$route,$cookies,$location,$window) {
-	$scope.IP=$cookies.IP;
-	$scope.protocol=$cookies.protocol;
-	if($scope.IP===undefined)
-	{
-		$window.location.href="#";
-		$route.reload();
-	}
-	else
-	{
-		$scope.queryAll=$location.search()['queryAll'];
-		if($scope.queryAll === undefined || $scope.queryAll === "")
-		{
-			$scope.num_results=0;
-			$scope.namespacesReposList=[];
-			$http({method: 'GET', url: $scope.protocol+'://'+$scope.IP+'/v1/search'}).success(function(data)
-			{
-				$scope.num_results=data.num_results;
-				$scope.namespacesReposList=data.results;
-			}).error(function(data){alert('Unable to request.')});
+
+function retrieveDockerHub( url , $http, $scope){
+	$http({method: "GET", url: url}).success(function(data){
+		$scope.num_results= $scope.num_results + data.count;
+		angular.forEach(data.results, function(result){
+		    starred = false
+		    if(result.star_count > 0){
+		       starred = true
+            }
+			$scope.dictionary["biodckr/" + result.name] = {domain: "biodckr/", name: result.name, description: result.description, lastModified: result.last_updated, number_pull: [result.pull_count, 15000], start_count:starred}
+			$scope.namespacesList.push({domain: "biodckr/", name: result.name, description: result.description, lastModified: result.last_updated, number_pull: [result.pull_count, 15000], start_count:starred})
+		});
+		if(data.next != null){
+			retrieveDockerHub(cross_proxy + data.next, $http, $scope)
 		}
-		else
-		{
-			$scope.IP=$cookies.IP;
-			console.log('the ip is ' + $scope.IP);
-			$scope.num_results=0;
-			$scope.namespacesReposList=[];
-			$http({method: 'GET', url: $scope.protocol+'://'+$scope.IP+'/v1/search'}).success(function(data)
-			{
-				$scope.num_results=data.num_results;
-				angular.forEach(data.results,function(result)
-				{
-					$scope.queryAll = angular.lowercase($scope.queryAll);
-					if( result.name.indexOf($scope.queryAll) >= 0 )
-						$scope.namespacesReposList.push(result);
-				});
-			}).error(function(data){alert('Unable to request.')});
-		}
-	}
-});
-app.controller('RepositoriesController', function($scope,$http,$location,$window,$cookies,$route) {
-	$scope.IP=$cookies.IP;
-	$scope.protocol=$cookies.protocol;
-	if($scope.IP===undefined)
-	{
-		$window.location.href="#";
-		$route.reload();
-	}
-	else
-	{
-		$scope.namespace=$location.search()['namespace'];
-		$scope.num_results=0;
-		$scope.repositoriesList=[];
-		$scope.go = function (path) {
-		$location.path(path);
-		};
-		$http({method: 'GET', url: $scope.protocol+'://'+$scope.IP+'/v1/search'}).success(function(data)
-		{
-			$scope.num_results=data.num_results;
-			results=data.results;
-			numRepos=0;
-			angular.forEach(results,function(result)
-			{
-				if(result.name.split('/')[0]===$scope.namespace)
-				{
-					temp={};
-					numRepos++;
-					temp['name']=result.name.split('/')[1];
-					temp['id']=numRepos;
-					$scope.repositoriesList.push(temp);
-				}
-			});		
-		}).error(function(data){alert('Unable to request.')});
-		deleteRepoURL=$scope.protocol+'://'+$scope.IP+'/v1/repositories/'+$scope.namespace;
-		$scope.deleteRepo = function (repo)
-		{
-				$http.delete(deleteRepoURL+'/'+repo +'/').success(function (data)
-				{
-					console.log('Deleted Repo : '+repo);
-					$window.location.href = "#showRepositories?namespace="+$scope.namespace;
-					$route.reload();
-				}).error(function(data){});;
-		};
-	}
-});
+	}).error(function(data){console.log("Unable to request the data")});
+}
+
+function retrieveQuayIO( url , $http, $scope){
+	$http({method: "GET", url: url, headers: {
+        'Authorization': "Bearer "+ "XRYLsxvQqmQLpP7RrajpFdiZntveNEyiffXyibK0"}}).success(function(data){
+		$scope.num_results= $scope.num_results + data.repositories.length;
+		angular.forEach(data.repositories, function(result){
+			$scope.dictionary["quay.io/biocontainers/" + result.name] = {domain: "quay.io/biocontainers/", name: result.name, description: result.description, lastModified: (result.last_modified * 1000), number_pull: [result.popularity, 50], start_count:result.is_starred}
+			$scope.namespacesList.push({domain: "quay.io/biocontainers/", name: result.name, description: result.description, lastModified: (result.last_modified * 1000), number_pull: [result.popularity, 50], start_count:result.is_starred})
+		});
+	});
+}
+
 app.controller('ImagesController', function($scope,$http,$location,$window,$cookies,$route) {
-	$scope.IP=$cookies.IP;
-	$scope.protocol=$cookies.protocol;
-	if($scope.IP===undefined)
-	{
-		$window.location.href="#";
-		$route.reload();
-	}
-	else
-	{
-		$scope.namespace=$location.search()['namespace'];
-		$scope.repository=$location.search()['repository'];
-		$scope.tagsList=[];
-		URL=$scope.protocol+'://'+$scope.IP+'/v1/repositories/'+$scope.namespace+'/'+$scope.repository+'/tags';
-		 $scope.getTextToCopy = function(tag_name) {
-			return "docker pull "+$scope.IP+"/"+$scope.namespace+"/"+$scope.repository+":"+tag_name;
-		}
-		$scope.doSomething = function () {
-			console.log("Text copied");
-		}
-		$scope.setNewTag = function (nTag)
-		{
-			$scope.newTag=nTag;
-		}
-		$scope.deleteTag = function (tag)
-		{
-				$http.delete(URL+'/'+tag).success(function (data)
-				{
-					console.log('Deleted tag : '+tag);
-					$window.location.href = "#showImages?namespace="+$scope.namespace+"&repository="+$scope.repository;
-					$route.reload();
-				}).error(function(data){alert('Unable to delete.')});;
-		};
-		deleteRepoURL=$scope.protocol+'://'+$scope.IP+'/v1/repositories/'+$scope.namespace+'/'+ $scope.repository;
-		$scope.deleteRepo = function ()
-		{
-				$http.delete(deleteRepoURL+'/').success(function (data)
-				{
-					console.log('Deleted Repo : '+$scope.repository);
-					$window.location.href = "#showRepositories?namespace="+$scope.namespace;
-					$route.reload();
-				}).error(function(data){});;
-		};
-		$scope.changeTag = function (oldTag,newTag)
-		{
-			$scope.oldTagURL=URL+'/'+oldTag;
-			$scope.newTagURL=URL+'/'+newTag;
-			$scope.imageID='';
-			imageId='\"hello\"';
-			console.log('image id fetch url is '+ $scope.oldTagURL);
-			console.log('tag putting url is ' + $scope.newTagURL);
-			console.log('new tag is '+ newTag);
-			if(newTag !== undefined)
-			{
-				$http({method: 'GET', url: $scope.oldTagURL }).success(function(data)
-				{
-					console.log('Data fetched : '+data);
-					imageId='\"'+data.replace(/"/g,'')+'\"';
-					if(oldTag !== newTag)
-					{
-						$http({method: 'PUT', url: $scope.newTagURL, data: imageId , headers: {"Content-Type": "application/json","Accept": "application/json"}}).success(function(data)
-						{
-							$http.delete($scope.oldTagURL).success(function (data)
-							{
-								$window.location.href = "#showImages?namespace="+$scope.namespace+"&repository="+$scope.repository;
-								$route.reload();
-							}).error(function(data){alert('Unable to delete.')});		
-						}).error(function(data){
-						console.log('Unable to set tag for imageID');
-						});
-					}
-					else
-					{
-						console.log('OldTag same as newTag ,no action taken');
-					}
-				}).error(function(data){console.log('Unable to get image ID for tag')});
-			}
-			else
-			{
-				console.log('New tag is empty. Not doing anything');
-			}	
-		}
-		$http({method: 'GET', url: URL }).success(function(data)
-		{
-			$scope.results=data;
-			numTags=0;
-			angular.forEach($scope.results,function(key,value)
-			{
-				numTags++;
-				temp={};
-				temp['name']=value;
-				temp['id']=numTags;
-				$scope.tagsList.push(temp);
-			});		
-		}).error(function(data){alert('Unable to request. Maybe target server is not supported CORS tag APIs.')});
-	}
+
+    $scope.domain=$location.search()['domain'];
+    $scope.repository=$location.search()['repository'];
+    $scope.repo = {};
+
+    if($scope.domain == "dockerhub"){
+        repoURL    = cross_proxy + dockerhub_registry + $scope.repository;
+        imagesURL  = repoURL + "/tags/";
+        dockerFile = repoURL + "/dockerfile/";
+        $http({method: "GET", url: repoURL}).success(function(data){
+            $scope.repo.repoName     = data.name;
+            $scope.repo.description  = data.description;
+            $scope.repo.star_count   = data.star_count;
+            $scope.repo.pull_count   = data.pull_count;
+            $scope.repo.last_updated = data.last_updated;
+            $scope.repo.dockerFile   = "";
+            $scope.repo.imagesList   = [];
+            $scope.repo.domain       = "docker";
+            $scope.repo.command      = "docker pull biodckr/" + $scope.repository;
+            $scope.repo.url          = "https://hub.docker.com/r/biodckr/" + $scope.repository;
+            $http({method: "GET", url: dockerFile}).success(function(data){
+                $scope.repo.dockerFile = data.contents;});
+            $http({method: "GET", url: imagesURL}).success(function(data){
+                angular.forEach(data.results, function(result){
+                    $scope.repo.imagesList.push({tag: result.name, last_updated: result.last_updated, size: result.full_size})
+                })
+            });
+
+        }).error(function(data){console.log("Unable to request the data")})
+    }
+    if($scope.domain == "quay"){
+        repoURL    = quay_registry_repo + quay_organization + $scope.repository;
+        $http({method: "GET", url: repoURL, headers: {
+            'Authorization': "Bearer "+ "XRYLsxvQqmQLpP7RrajpFdiZntveNEyiffXyibK0"}}).success(function(data){
+            $scope.repo.repoName     = $scope.repository;
+            $scope.repo.description  = data.description;
+            $scope.repo.domain       = "quay";
+            $scope.repo.url          = "https://quay.io/organization/biocontainers" + $scope.repository;
+            $scope.repo.command      = "docker pull quay.io/biocontainers/" + $scope.repository
+            angular.forEach(data.tags, function(key, value){
+                    $scope.repo.imagesList.push({tag: value.name, last_updated: value.last_modified, size: value.size})
+                })
+            });
+    }
+});
+
+
+app.filter('filterByName', function() {
+    return function(items, field) {
+        var result = {};
+        angular.forEach(items, function(value, key) {
+            if (!value.hasOwnProperty(field)) {
+                result[key] = value;
+            }
+        });
+        return result;
+    };
+});
+
+app.filter('orderObjectBy', function() {
+    return function(items, field, reverse) {
+        var filtered = [];
+        angular.forEach(items, function(item) {
+            filtered.push(item);
+        });
+        filtered.sort(function (a, b) {
+            return (a[field] > b[field] ? 1 : -1);
+        });
+        if(reverse) filtered.reverse();
+        return filtered;
+    };
 });
