a7774c2823cf2b471aee60332b36dd84fead82e0
Ling Xu <lingxu@c02wp03uhtdg.windows.ebi.ac.uk>
2018-10-09 09:06:40 +0100

    update nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,44 +1,49 @@
 <template>
-    <Menu mode="horizontal" active-name="1" @on-select="menuSlect">
-        <a class="logo"><img src="static/logo/biocontainers-logo.png" width="40" height="40">BioContainers Registry UI</a>
-        <div class="menu">
-            
-            <MenuItem name="statslink">
-                <Icon type="ios-home-outline"></Icon>
-                Stats Link
-            </MenuItem>
-            <MenuItem name="multipackage">
-                <Icon type="ios-home-outline"></Icon>
-                Multi-package
-            </MenuItem>
-            <MenuItem name="biocontainers">
-                <Icon type="ios-home-outline"></Icon>
-                BioContainers
-            </MenuItem>
-            <MenuItem name="bithub">
-                <Icon type="social-github"></Icon>
-                GitHub
-            </MenuItem>
-        </div>
-    </Menu>
+    <div class="nav-container">
+        <Menu mode="horizontal" active-name="1" @on-select="menuSlect">
+            <a class="logo" @click="gotoHome"><img src="static/logo/biocontainers-logo.png" width="40" height="40">BioContainers Registry UI</a>
+            <div class="menu">
+                <MenuItem name="statslink">
+                   <Icon type="ios-home-outline"></Icon>
+                    Stats Link
+                </MenuItem>
+                <MenuItem name="multipackage">
+                    <Icon type="ios-home-outline"></Icon>
+                    Multi-package
+                </MenuItem>
+                <MenuItem name="biocontainers">
+                    <Icon type="ios-home-outline"></Icon>
+                    BioContainers
+                </MenuItem>
+                <MenuItem name="github">
+                    <Icon type="social-github"></Icon>
+                    GitHub
+                </MenuItem>
+            </div>
+        </Menu>
+    </div>
 </template>
 <script>
     export default {
         data () {
             return {
-               
+                
             }
         },
-    methods:{
+        methods:{
+            gotoHome(){
+                this.$router.push({name:'Index'});
+            },
             menuSlect(name){
                 switch(name){
                   case 'statslink':
-                    this.$router.push({name:'index'});
+                    console.log(123);
+                    this.$router.push({name:'MappingData'});
                     //this.activeName = 'index';
                   break;
 
                   case 'multipackage':
-                    window.open('https://github.com/BioContainers/multi-package-containers');
+                    window.open('http://biocontainers.pro/multi-package-containers/');
                     //this.activeName = 'discover';
                   break;
                   case 'biocontainers':
@@ -52,13 +57,13 @@
                 }
             } 
         }
-
-
-
-
+        
     }
 </script>
 <style scoped>
+    .nav-container{
+        background-color: rgba(240,240,240,.95);
+    }
     .logo{
         float: left;
         color: #495060;
@@ -76,5 +81,9 @@
         float: right;
     }
 </style>
-
+<style>
+    .nav-container .ivu-menu-light{
+        background: rgba(240,240,240,.95) !important;
+    }
+</style>
 
