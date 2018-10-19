cd4d12820b77bf9383533fc280419c2102b71811
xuling <xuling@JingwendeMacBook.local>
2018-10-19 21:23:22 +0800

    add retrieveQuayIO API

--- src/components/Mappingdata.vue
+++ src/components/Mappingdata.vue
@@ -44,6 +44,12 @@ export default {
   name: 'MappingData',
   data () {
     return {
+        QUAY_ORGANIZATION:"https://quay.io/api/v1/repository?namespace=biocontainers&popularity=true&last_modified=true",
+        RetrieveGitHubIssuesAPI1:"https://api.github.com/repos/biocontainers/containers/issues?state=all&per_page=100",
+        RetrieveGitHubIssuesAPI2:"https://api.github.com/repos/biocontainers/specs/issues?state=all&per_page=100",
+        RetrieveGitHubIssuesAPI3:"https://api.github.com/repos/biocontainers/specs/issues/comments?per_page=100",
+        RetrieveGitHubIssuesAPI4:"https://api.github.com/repos/biocontainers/containers/issues/comments?per_page=100",
+        dates:{},
         keywords:'',
         resultsTableCol:[
             {
@@ -165,11 +171,58 @@ export default {
 
             });
     },
+    retrieveGitHubIssues(){
+      let promise1 = this.$http.get(this.RetrieveGitHubIssuesAPI1);
+      let promise2 = this.$http.get(this.RetrieveGitHubIssuesAPI2)
+      let promise3 = this.$http.get(this.RetrieveGitHubIssuesAPI3)
+      let promise4 = this.$http.get(this.RetrieveGitHubIssuesAPI4)
+           
+      Promise.all([promise1, promise2, promise3, promise4]).then(([v1,v2,v3,v4]) => {
+        console.log('v1',v1);
+        console.log('v2',v2);
+        console.log('v3',v3);
+        console.log('v4',v4);
+
+        // Process of merging results v1 and v2 together here
+      })
+    },
+    retrieveQuayIO(){
+      this.$http
+            .get(this.QUAY_ORGANIZATION,{headers: {'X-Requested-With' :'XMLHttpRequest','Authorization': "Bearer "+ "XRYLsxvQqmQLpP7RrajpFdiZntveNEyiffXyibK0"}})
+            .then(function(res){
+              console.log('retrieveQuayIO',res);
+              let repositories = res.body.repositories
+                for(let i in repositories){
+                   let item = {
+                      domain: "quay.io/biocontainers/",
+                      name: repositories[i].name, 
+                      description: repositories[i].description, 
+                      lastModified: repositories[i].last_modified, 
+                      number_pull: [repositories[i].popularity, 50], 
+                      start_count:repositories[i].is_starred
+                   }
+
+                   let num = Math.floor(repositories[i].last_modified/1000)
+                   if(!isNaN(num))
+                      this.dates[num.toString()] = this.dates[num.toString()] ? this.dates[num.toString()]+1:1;
+                }
+
+              
+
+                console.log('this.dates',this.dates);
+            },function(err){
+              console.log('err',err);
+            });
+    },
     search(){
         console.log('search');
+        
     }
   },
   mounted(){
+
+    this.retrieveQuayIO();
+    this.retrieveGitHubIssues();
     //this.test();
   }
 }
